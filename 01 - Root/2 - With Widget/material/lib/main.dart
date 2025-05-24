94f6eb2c0f58868845993046afaaa3ee4aa7af15
Rick Beerendonk <rick@beerendonk.com>
2025-05-24 22:36:24 +0200

    Add material version.

---
+++ 01 - Root/2 - With Widget/material/lib/main.dart
@@ -0,0 +1,15 @@
+// European Union Public License version 1.2
+// Copyright © 2020 Rick Beerendonk
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Scaffold(body: Text('Hello Flutter!')));
+  }
+}
