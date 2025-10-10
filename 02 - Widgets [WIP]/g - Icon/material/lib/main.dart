b6d035b9ec53b6cfb936bccb7ef2dd684b38748b
Rick Beerendonk <rick@beerendonk.com>
2025-10-10 22:35:09 +0200

    Fix.

---
+++ 02 - Widgets [WIP]/g - Icon/material/lib/main.dart
@@ -0,0 +1,10 @@
+// European Union Public License version 1.2
+// Copyright © 2025 Rick Beerendonk
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    Directionality(textDirection: TextDirection.ltr, child: Icon(Icons.star)),
+  );
+}
