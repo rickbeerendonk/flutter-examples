734c3fe8e5dd1c653843a3f8eb1cc6ad5f05f60f
Rick Beerendonk <rick@beerendonk.com>
2025-10-10 22:30:53 +0200

    Add "Widgets - Placeholder" example.

--- 02 - Widgets [WIP]/i - Placeholder/widgets/lib/main.dart
+++ 02 - Widgets [WIP]/i - Placeholder/widgets/lib/main.dart
@@ -1,12 +1,5 @@
 import 'package:flutter/widgets.dart';
 
 void main() {
-  runApp(
-    Placeholder(
-      color: Colors.green,
-      strokeWidth: 4,
-      fallbackWidth: 200,
-      fallbackHeight: 200,
-    ),
-  );
+  runApp(Placeholder());
 }
