0b8a816608985efb1860e5323282f351c9b7fcbc
Rick Beerendonk <rick@beerendonk.com>
2025-10-10 21:17:39 +0200

    Add "Widgets - Image" example.

--- 02 - Widgets [WIP]/f - Image/widgets/lib/main.dart
+++ 02 - Widgets [WIP]/f - Image/widgets/lib/main.dart
@@ -1,9 +1,13 @@
+// European Union Public License version 1.2
+// Copyright © 2025 Rick Beerendonk
+
 import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(
-    Image.network(
-      'https://flutter.dev/images/flutter-logo-sharing.png',
+    // Image should be registered in pubspec.yaml
+    Image.asset(
+      './assets/images/Oblicum-logo-400.png',
       width: 200,
       height: 200,
     ),
