518b9e04eab083cae290041bf691971bdadb300b
Rick Beerendonk <rick@beerendonk.com>
2025-10-15 08:56:54 +0200

    Add "Solution".

---
+++ 05 - List Rendering/b2- Index as Key Issue (solution)/widgets/lib/main.dart
@@ -0,0 +1,98 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2021 Rick Beerendonk          !*/
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Index as Key Issue',
+      home: Scaffold(
+        appBar: AppBar(title: Text('Index as Key Issue')),
+        body: App(),
+      ),
+    );
+  }
+}
+
+class App extends StatefulWidget {
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  List<String> items = ['one', 'two', 'three'];
+  Map<String, TextEditingController> controllers = {};
+
+  @override
+  void initState() {
+    super.initState();
+    // Initialize controllers for existing items
+    for (var item in items) {
+      controllers[item] = TextEditingController();
+    }
+  }
+
+  @override
+  void dispose() {
+    // Dispose all controllers
+    for (var controller in controllers.values) {
+      controller.dispose();
+    }
+    super.dispose();
+  }
+
+  void insertTop() {
+    setState(() {
+      final newItem = (items.length + 1).toString();
+      items = [newItem, ...items];
+      // Create controller for new item
+      controllers[newItem] = TextEditingController();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Add some text in an input and observe what happens if you click the button.',
+            style: TextStyle(color: Colors.grey),
+          ),
+          SizedBox(height: 16),
+          ElevatedButton(
+            onPressed: insertTop,
+            child: Text('Insert at Top'),
+          ),
+          SizedBox(height: 16),
+          Expanded(
+            child: ListView.builder(
+              itemCount: items.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  key: ValueKey(
+                      items[index]), // Use item value as key to avoid issues
+                  title: TextField(
+                    controller: controllers[items[index]],
+                    decoration: InputDecoration(
+                      hintText: items[index],
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
