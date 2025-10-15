2cf575f9b1430e9a77b10b5d6e2656ef7fb4b8a3
Rick Beerendonk <rick@beerendonk.com>
2025-10-15 02:45:02 +0200

    Reorder.

--- 05 - Databinding/b - RichText/widgets/lib/main.dart
+++
@@ -1,9 +0,0 @@
-// European Union Public License version 1.2
-// Copyright © 2020 Rick Beerendonk
-
-import 'package:flutter/widgets.dart';
-
-void main() {
-  const _name = 'Flutter';
-  runApp(Text('Hello $_name!', textDirection: TextDirection.ltr));
-}
