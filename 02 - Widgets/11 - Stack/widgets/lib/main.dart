8170d0cdad2a2c69541610ffa7c2df2a922ea0fc
Rick Beerendonk <rick@beerendonk.com>
2025-10-15 03:10:14 +0200

    Use Positioned.

--- 02 - Widgets/11 - Stack/widgets/lib/main.dart
+++ 02 - Widgets/11 - Stack/widgets/lib/main.dart
@@ -11,7 +11,15 @@ void main() {
         // Stack widget allows you to overlay multiple widgets on top of each other.
         // The first child is at the bottom, the last child is on top.
         alignment: Alignment.center,
-        children: const [Text('1'), Text('2'), Text('3')],
+        children: const [
+          Text('1'),
+          Text('2'),
+          const Positioned(
+            right: 50,
+            top: 50,
+            child: Text('3'),
+          ),
+        ],
       ),
     ),
   );
